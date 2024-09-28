1b4ef0534dbacbbc5b08af9c5b8f5117a8c4a2ea
paivaNick <nipaiva93@gmail.com>
2024-09-28 16:44:10 -0300

    aula2- resoulaçao do desafio

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'ola, mundo!'}
+
+
+@app.get('/message', status_code=HTTPStatus.OK, response_model=Message)
+def message():
+    return {'message': 'oi'}
+
+
+@app.get('/ola', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def ola_html():
+    return """<h1>ola, mundo! </h1>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,8 +5,20 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
+# triple A
 def test_read_root_return_ok_ola_mundo():
+    # arrange
     client = TestClient(app)
+    # act
     response = client.get('/')
+    # asserts
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'ola, mundo!'}
+
+
+def test_ola_html_return_ok_ola():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == """<h1>ola, mundo! </h1>"""
